df817db16cfc7f80d58798b45e9956046007752f
nirmit jasapara <nirmit.jasapara@sjsu.edu>
2020-03-23 14:54:31 -0700

    added background

---
+++ store.js
@@ -0,0 +1,56 @@
+const STORE = {
+    questions: [
+      {
+        question: "How much mass is in our universe?",
+        options: [
+          "about 10^15 kg", 
+          "about 10^42 kg", 
+          "about 10^53 kg", 
+          "about 10^73 kg"
+        ],
+        answer: "about 10^53 kg"
+      },
+      {
+        question: "How big is in our universe?",
+        options: [
+          "about 10^70 m^3",
+          "about 10^80 m^3",
+          "about 10^113 m^3",
+          "about 10^150 m^3"
+        ],
+        answer: "about 10^80 m^3"
+      },
+      {
+        question: "How big is a Planck length?",
+        options: [
+          "about 10^-12 m",
+          "about 10^-21 m",
+          "about 10^-35 m",
+          "about 10^-55 m"
+        ],
+        answer: "about 10^-35 m"
+      },
+      {
+        question: "How big is a Planck mass?",
+        options: [
+          "about 10^-8 kg",
+          "about 10^-21 kg",
+          "about 10^-32 kg",
+          "about 10^-53 kg"
+        ],
+        answer: "about 10^-8 kg"
+      },
+      {
+        question: "How big is a Planck time?",
+        options: [
+          "about 10^-9 s",
+          "about 10^-11 s",
+          "about 10^-13 s",
+          "about 10^-44 s"
+        ],
+        answer: "about 10^-44 s"
+      },
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
